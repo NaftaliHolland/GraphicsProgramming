f941e2c41f7de9c3b12b362f1d95dc0c3ca0d526
Naftali Holland <naftaliholland01@gmail.com>
2024-02-02 14:32:12 +0300

    SDL

---
+++ mySDL/01_hello_SDL.cpp
@@ -0,0 +1,46 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+/** Screen dimensions **/
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char *args[])
+{
+	/** Initialize window **/
+	SDL_Window* window = NULL;
+
+	/** Surface on the window **/
+	SDL_Surface* screenSurface = NULL;
+
+	/** initialize SDL **/
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+		printf("SDL could not initialize! SDL_ERROR: %s\n", SDL_GetError());
+	else
+	{
+		/** Create Window **/
+		window = SDL_CreateWindow("My first SDL program", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		else
+		{
+			/** Get window surface **/
+			screenSurface = SDL_GetWindowSurface(window);
+
+			/** fill the surface white **/
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			/** Update surface **/
+			SDL_UpdateWindowSurface(window);
+
+			/*** Get window to stay up **/
+			SDL_Event e; bool quit = false; while (quit == false ){while (SDL_PollEvent(&e)){if (e.type == SDL_QUIT) quit = true;}}}}
+
+	/** Destroy window **/
+	SDL_DestroyWindow(window);
+
+	/** Quit SDL **/
+	SDL_Quit();
+	return (0);
+}
